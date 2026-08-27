2c5e1ac212927e7e1c79a1a35a7cccd3041b0234
Jesus Gonzalez <jesuslibros19@gmail.com>
2026-08-26 18:30:06 -0600

    Create real_python_habits.py

---
+++ real_python_habits.py
@@ -0,0 +1,41 @@
+class Habitos:
+
+    def __init__(self, nombre: str) -> None:
+        self.nombre: str = nombre
+        self.semana: dict[int, dict[str, bool]] = {}
+  
+    def iniciar_dias_semana(self) -> dict[str, bool]:
+        """ 
+        Input: NA
+        Return: Regresa un diccionario con {Dia de la semana: Boolean}
+        """
+        temp_dic: dict[str, bool] = {"Lunes": False, 
+                                     "Martes": False,
+                                     "Miercoles": False,
+                                     "Jueves": False,
+                                     "Viernes": False,
+                                     "Sabado": False,
+                                     "Domingo": False}
+        return temp_dic
+
+    def crear_semana(self, num_semana: int):
+        try:
+            semana = int(num_semana)
+            self.semana.update({semana: self.iniciar_dias_semana()})
+        except Exception as e:
+            print(f"No se pudo agregar la semana, error: {e}")
+
+
+ejercicio = Habitos("Ejercicio")
+
+# crear semanas
+ejercicio.crear_semana(34)
+ejercicio.crear_semana(35)
+ejercicio.crear_semana(36)
+
+# checar que no se pueda repetir la semana
+ejercicio.crear_semana(34)
+ejercicio.crear_semana("qwe")
+
+# imprimir semana
+print(ejercicio.semana)
