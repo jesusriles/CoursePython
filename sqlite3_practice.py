f0cad9172d93ae602b7ce1b29f384ee0af28be2d
Jesus Gonzalez <jesuslibros19@gmail.com>
2026-09-22 13:38:07 -0600

    Everything in functions
    Everything moved into functions.

--- sqlite3_practice.py
+++ sqlite3_practice.py
@@ -1,20 +1,28 @@
 import sqlite3
 import datetime
+from typing import Any
 
 _PATH_DB = "financesx.db"
 
-# get today date
-current_year = datetime.datetime.now().year
-current_month = datetime.datetime.now().month
-current_day = datetime.datetime.now().day
-today = str(current_day) + "-" + str(current_month) + "-" + str(current_year)
 
-# create database
-conn = sqlite3.connect(_PATH_DB)
-cursor = conn.cursor()
+def get_today_date() -> str:
+    ''' Return today |date as DD-MM-YYYY '''
+    current_year: int = datetime.datetime.now().year
+    current_month: int = datetime.datetime.now().month
+    current_day: int = datetime.datetime.now().day
+    return f"{current_day}-{current_month}-{current_year}"
 
-# create table
-cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+
+def create_database() -> None:
+    conn = sqlite3.connect(_PATH_DB)
+    conn.close()
+
+
+def insert_register() -> int | None:
+    conn = sqlite3.connect(_PATH_DB)
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
     date TEXT,
     name TEXT, 
     amount INTEGER,
@@ -26,42 +34,35 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS users (
     comments TEXT)
     """)
 
-# insert information
-query = """INSERT INTO users VALUES (
+    query = """INSERT INTO users VALUES (
     ?, ?, ?, ?, ?, ?, ?, ?, ?
-    );""" # 9
+    );"""  # 9
 
-# insert the data into table
-cursor.execute(query, (
-    today, 
-    "Costco", 
-    7003.94,
-    0,
-    today,
-    "HSBC Viva",
-    "Alimentos",
-    "Despensa",
-    "NA")
+    cursor.execute(query, (
+        today, 
+        "Costco", 
+        7003.94,
+        0,
+        today,
+        "HSBC Viva",
+        "Alimentos",
+        "Despensa",
+        "NA")
     )
-conn.commit()
-
-# select information and print
-cursor.execute('SELECT rowid, * FROM users')
-
-result = cursor.fetchall()
-for row in result:
-    print(row)
+    conn.commit()
+    return cursor.lastrowid
 
 
-'''
-Id	
-Fecha
-Concepto
-Cantidad
-Pagado (Verdadero o Falso)
-Fecha en la que se pago (Fecha como TEXTO)
-Forma de pago (Especificar la tarjeta o Efectivo)
-Categoria (Alimentos, Transportes, etc.)
-Subcategoria (Restaurantes, etc.)
-Comentarios (Cualquier comentario)
-'''
+def return_all_registers() -> list[Any]:
+    conn = sqlite3.connect(_PATH_DB)
+    cursor = conn.cursor()
+    cursor.execute('SELECT rowid, * FROM users')
+    return cursor.fetchall()
+
+
+today = get_today_date()
+create_database()
+insert_register()
+
+for x in return_all_registers():
+    print(x)
